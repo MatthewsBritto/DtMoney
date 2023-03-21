95142ef05db14e6901479ed6ea96986c89ee2236
unknown <luismatthews29@gmail.com>
2023-03-21 17:53:02 -0300

    ini table

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,5 +1,6 @@
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
+import { TransactionsContainer } from "./styles";
 
 
 
@@ -8,6 +9,26 @@ export function Transactions() {
       <div>
          <Header />
          <Summary />
+
+      <TransactionsContainer>
+         <table>
+               <tbody>
+                  <tr>
+                     <td width="50%">Desenvolvimento de site</td>
+                     <td>R$ 12.000,00</td>
+                     <td>Venda</td>
+                     <td>13/02/2022</td>
+                  </tr>
+                  <tr>
+                     <td width="50%">Lanchão</td>
+                     <td>- R$ 50,00</td>
+                     <td>Compra</td>
+                     <td>13/02/2022</td>
+                  </tr>
+               
+               </tbody>
+         </table>
+      </TransactionsContainer>
       </div>
    )
 }
